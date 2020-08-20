b9aa16576f4dc8e4cbba1a4d1ea0d8a7bc51fb48
David Bustos <davidbustos@Davids-MacBook-Pro.local>
2020-08-20 14:48:00 -0500

    Ejemplo mappeo con dictionary y objetos anidados y colecciones de objetos

---
+++ PruebaMappeoDictionary/Controllers/PruebaMappingDictionaryController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PruebaMappeoDictionary.Models;
+
+namespace PruebaMappeoDictionary.Controllers
+{
+    public class PruebaMappingDictionaryController : Controller
+    {
+        public IActionResult Index()
+        {
+            PruebaMappingDictionaryViewModel vm = new PruebaMappingDictionaryViewModel
+            {
+                SubClassMappingDictionary = new System.Collections.Generic.Dictionary<string, SubClassMappingDictionary>
+                {
+                    { Guid.NewGuid().ToString(),
+                        new SubClassMappingDictionary{
+                            StringProp = "string prop",
+                            SubSubClassMappingDictionary = new System.Collections.Generic.Dictionary<string, SubSubClassMappingDictionary>
+                            {
+                                { Guid.NewGuid().ToString(), new SubSubClassMappingDictionary{
+                                    StrField = "Campo de texto 1"
+                                } },
+                                { Guid.NewGuid().ToString(), new SubSubClassMappingDictionary{
+                                    StrField = "Campo de texto 2"
+                                }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Index(PruebaMappingDictionaryViewModel model)
+        {
+            return Json(model);
+        }
+    }
+}
---
+++ PruebaMappeoDictionary/Models/PruebaMappingDictionaryViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace PruebaMappeoDictionary.Models
+{
+    public class PruebaMappingDictionaryViewModel
+    {
+        //Guid.NewGuid().ToString();
+        public Dictionary<string, SubClassMappingDictionary> SubClassMappingDictionary { get; set; }
+    }
+    public class SubClassMappingDictionary
+    {
+        [Display(Name = "Propiedad")]
+        public string StringProp { get; set; }
+        public Dictionary<string, SubSubClassMappingDictionary> SubSubClassMappingDictionary { get; set; }
+    }
+    public class SubSubClassMappingDictionary
+    {
+        public string StrField { get; set; }
+    }
+}
